513f5d9a1e3d91e02f7cb1560f402fbce977ff3a
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-07 19:07:07 +0300

    moviesection sayfası eklendi

--- app/page.jsx
+++ app/page.jsx
@@ -6,12 +6,21 @@ import Genres from "@/mocks/genres.json";
 
 import { FeaturedMovie } from "@/components/featured-movie";
 import { Categories } from "@/components/categories";
+import { MoviesSection } from "@/components/movie-section";
 
 function HomePage() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      <MoviesSection
+        title="Populer Films"
+        movies={Movies.results.slice(1, 7)}
+      />
+      <MoviesSection
+        title="Your Favorites"
+        movies={Movies.results.slice(7, 13)}
+      />
     </div>
   );
 }
