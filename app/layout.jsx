95aee3194e28c6675f82de5f778acb040c11dd28
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-08 14:38:50 +0300

    head sayfası oluşturuldu ve layout yeniden düzenlendi

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,17 @@
 import { Inter } from "@next/font/google";
 
 import "@/styles/reset.css";
+import "@/styles/variables.css";
 import "@/styles/global.css";
 
-import Header from "@/components/header/index";
-import Footer from "@/components/footer/index";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 const interFontFamily = Inter({ subsets: ["latin"] });
 
-function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="tr" className={interFontFamily}>
+    <html lang="en" className={interFontFamily.className}>
       <body className="container">
         <Header />
         <main>{children}</main>
@@ -19,5 +20,3 @@ function RootLayout({ children }) {
     </html>
   );
 }
-
-export default RootLayout;
